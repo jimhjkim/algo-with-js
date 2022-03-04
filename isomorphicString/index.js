9c689cc2fd2877a74cc9ff719a278e5a0e70fcbd
Jim Kim <jim.hyunjin.kim@gmail.com>
2022-03-04 15:51:02 -0500

    Determine if two strings are isomorphic

---
+++ isomorphicString/index.js
@@ -0,0 +1,28 @@
+/**
+ * Given two strings, determine if they are isomorphic. Two strings are "isomorphic"
+ * if each unique character from the first string can be replaced to match the second
+ * string, without changing the ordering of the characters.
+ */
+const isIsomorphic = (str1, str2) => {
+  const mapping = {};
+  const used = new Set();
+  let i;
+  if (str1.length !== str2.length) return false;
+  for (i = 0; i < str1.length; i++) {
+    char1 = str1[i];
+    char2 = str2[i];
+    if (char1 in mapping) {
+      if (mapping[char1] !== char2) return false;
+    } else {
+      if (used.has(char2)) return false;
+      mapping[char1] = char2;
+      used.add(char2);
+    }
+  }
+  return true;
+};
+
+const test1 = isIsomorphic("egg", "all"); // true
+console.log(test1);
+const test2 = isIsomorphic("wow", "aaa"); // false
+console.log(test2);
