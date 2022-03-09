2df6f6d6af9ed7039ffb02cc2dfa156db4229013
Jim Kim <jim.hyunjin.kim@gmail.com>
2022-03-09 09:35:54 -0500

    Naive solution to find kth largest number in array

---
+++ google/findKthLargest.js
@@ -0,0 +1,16 @@
+/**
+ * Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not necessarily the kth distinct element.
+ */
+
+// Naive solution (O(nlogn))
+const findKthLargest = (nums, k) => {
+    const sorted = nums.sort((a, b) => b - a);
+    return sorted[k - 1];
+}
+
+
+const test1 = findKthLargest([3, 2, 1, 5, 6, 4], 2);
+console.log(test1); // 5
+
+// const test2 = findKthLargest([3,2,3,1,2,4,5,5,6], 4);
+// console.log(test2); // 4
